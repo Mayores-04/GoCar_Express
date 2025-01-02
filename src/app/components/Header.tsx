d759d912ef410e3e760cde45a12ed3b47bd6b282
Jake <jakemayores05@gmail.com>
2025-01-02 21:53:19 +0800

    ready to deploy

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
         <div className="flex space-x-6">
           <div className="">
             <Image
-              src="/images/blueLogo.PNG"
+              src="/images/blueLogo.png"
               alt="logo"
               width={80}
               height={80}
